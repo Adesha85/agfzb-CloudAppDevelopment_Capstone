486d03d49d165828fdf1be1808006e9c78f5bb5c
Adesha Demmin <adeshademmin@yahoo.com>
2024-02-22 15:46:25 -0500

    Changes made today

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -11,12 +11,14 @@ class CarModelInline(admin.StackedInline):
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ('car_make','dealer_id','model','car_type','year')
+    list_display = ('name', 'model_type', 'year')
+    list_filter = ['year']
+    search_fields = ['name', 'model_type']
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
-    list_display = ('car_make','description','country')
+    list_display = ('name','description')
 
 
 # Register models here
